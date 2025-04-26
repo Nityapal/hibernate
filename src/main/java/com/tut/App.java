5afa283f3fc69653e4116b4d13fc3059b6dba916
Nitya <22bch036@nith.ac.in>
2025-04-26 08:34:05 +0530

    saving image and other type values with hibernate

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -1,5 +1,9 @@
 package com.tut;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,7 +14,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "project started!" );
         
@@ -22,15 +26,30 @@ public class App
         st.setName("bebe");
         st.setCity("hyd");
         System.out.println(st.toString());
+        
+        Address ad= new Address();
+        ad.setStreet("street1");
+        ad.setCity("Jodhpur");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(1234.14);
+        
+        //reading image
+        FileInputStream fis= new FileInputStream("src/main/java/cs1.jpg");
+        byte[] data= new byte[fis.available()];
+        fis.read(data);
+        ad.setImage(data);
+        
         Session session= factory.openSession();
         ///
-        
         session.beginTransaction();
         session.save(st);
+        session.save(ad);
         
         session.getTransaction().commit();
         
         session.close();
+        System.out.println("done....");
         
         ///
         String s = factory.toString();
