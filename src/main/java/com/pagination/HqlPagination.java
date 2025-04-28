7e1e49833f7c10ce2caf6451d6b297ee97975509
Nitya <22bch036@nith.ac.in>
2025-04-28 16:52:57 +0530

    implementing pagination

---
+++ src/main/java/com/pagination/HqlPagination.java
@@ -0,0 +1,34 @@
+package com.pagination;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.tut.Student;
+
+public class HqlPagination {
+	public static void main(String[] args) {
+		SessionFactory factory= new Configuration().configure().buildSessionFactory();
+		//SessionFactory factory= cfg.buildSessionFactory();
+		
+		//pagination
+		Session s= factory.openSession();
+		Query q1= s.createQuery("from Student");
+		
+		//implementing pagination
+		q1.setFirstResult(0); //start posn.
+		q1.setMaxResults(5); //end posn. or size
+		
+		List<Student> ls =q1.list();
+		for(Student st: ls) {
+			System.out.println(st.getName()+" : "+st.getCity());
+		}
+		
+		
+		
+		factory.close();
+	}
+}
