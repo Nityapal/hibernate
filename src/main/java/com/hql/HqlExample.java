037751e817b1e3ebf200c2c921f7ce234700d272
Nitya <22bch036@nith.ac.in>
2025-04-28 16:23:32 +0530

    some more hql queries like delete, update and join

--- src/main/java/com/hql/HqlExample.java
+++ src/main/java/com/hql/HqlExample.java
@@ -1,9 +1,11 @@
 package com.hql;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
@@ -26,6 +28,31 @@ public class HqlExample {
 			System.out.println(st.getName());
 		}
 		
+		Transaction tx= s.beginTransaction(); 
+		
+		//delete query
+//		Query q2= s.createQuery("delete from Student where city=:c");
+//		q2.setParameter("c", "knp");
+//		int r= q2.executeUpdate();
+//		
+//		System.out.println("deleted: ");
+//		System.out.println(r);
+		
+		//update query
+//		Query q3= s.createQuery("update Student set city=:c where name=:n");
+//		q3.setParameter("c","Delhi");
+//		q3.setParameter("n","Peter");
+//		int r= q3.executeUpdate();
+//		System.out.println(r+"obj updated");
+		
+		tx.commit();
+		
+		//join query
+		Query q4= s.createQuery("select q.ques , q.qid, a.answer from Question as q INNER JOIN q.answers as a");
+		List<Object[]> res= q4.getResultList();
+		for(Object[] arr: res) {
+			System.out.println(Arrays.toString(arr));
+		}
 		
 		s.close();
 		factory.close();
